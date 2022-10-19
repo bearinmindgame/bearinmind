96997a87089d1e096dd763ac7e30f78337adce50
Filotás Dénes <a9cqz0@inf.elte.hu>
2022-10-19 14:59:02 +0200

    Clean up code for main window

--- src/bearinmind/MainWindow.java
+++ src/bearinmind/MainWindow.java
@@ -1,25 +1,17 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bearinmind;
 
 import java.io.IOException;
 import javax.swing.JFrame;
 
 /**
- *
- * @author Majercsik Szabolcs
+ * Main window for the game.
  */
 public class MainWindow extends JFrame {
     public MainWindow() throws IOException {
-        setTitle("Unnamed 2D Adventure Game");
+        setTitle("Bear in Mind");
         setSize(500, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        
+        setAutoRequestFocus(true);
     }
-    
 }
