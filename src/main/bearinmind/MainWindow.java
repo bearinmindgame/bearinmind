1e8649ca9a017f96979a5269c19e36e8dbe30369
Salamon Kornél Soma <glj0ov@inf.elte.hu>
2022-11-18 17:59:48 +0100

    Add KeyListener methods

--- src/main/bearinmind/MainWindow.java
+++ src/main/bearinmind/MainWindow.java
@@ -4,15 +4,16 @@ import java.io.IOException;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import bearinmind.model.GameModel;
 
 /**
  * Main window for the game.
  */
-public class MainWindow extends JFrame {
+public class MainWindow extends JFrame implements KeyListener {
 
     private GameModel model;
-
     public MainWindow() throws IOException {
         initializeComponents();
         model = new GameModel();
@@ -37,5 +38,30 @@ public class MainWindow extends JFrame {
                 g.drawImage(model.getMap().terrainAt(j, i).image, j * w, i * h, w, h, this);
             }
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_W) {
+            model.getPlayer().moveUp();
+        } else if (e.getKeyCode() == KeyEvent.VK_S) {
+            model.getPlayer().moveDown();
+        } else if (e.getKeyCode() == KeyEvent.VK_A) {
+            model.getPlayer().moveLeft();
+        } else if (e.getKeyCode() == KeyEvent.VK_D) {
+            model.getPlayer().moveRight();
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        System.out.println("Key released : " + e.getKeyCode());
+        
     }
 }
