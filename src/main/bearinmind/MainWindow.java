ad9d3d36813be7e9f72befc132a7dd2188c637c5
Majercsik Szabolcs <szabi193@inf.elte.hu>
2022-12-23 09:36:36 +0100

    map changes size

--- src/main/bearinmind/MainWindow.java
+++ src/main/bearinmind/MainWindow.java
@@ -5,6 +5,7 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 
 import bearinmind.model.GameModel;
+import javax.swing.BoxLayout;
 
 /**
  * Main window for the game.
@@ -23,21 +24,28 @@ public class MainWindow extends JFrame {
         setSize(500, 500);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setAutoRequestFocus(true);
         
-        setResizable(false);
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+        
+        //setAutoRequestFocus(true);
+        //setResizable(false);
     }
 
     @Override
     public void paint(Graphics g) {
+        
+        
         super.paint(g);
-        int w = model.getMap().width;
-        int h = model.getMap().height;
-        setSize(w * 40, h * 40);
+        int width = model.getMap().width;
+        int height = model.getMap().height;
+        int scaleWidth = this.getSize().width / model.getMap().width;
+        int scaleHeight = this.getSize().height / model.getMap().height;
+        
+        setSize(width * 40, height * 40);
         
         for (int i = 0; i < model.getMap().height; i++) {
             for (int j = 0; j < model.getMap().width; j++) {
-                g.drawImage(model.getMap().terrainAt(j, i).image, j * w, i * h, w, h, this);
+                g.drawImage(model.getMap().terrainAt(j, i).image, j * scaleWidth, i * scaleHeight, scaleWidth, scaleHeight, this);
             }
         }
         
